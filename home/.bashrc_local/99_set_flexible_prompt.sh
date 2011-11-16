1de1b751d5d330f819b385bf182b071a72baefea
John Miller <jmiller@trustwave.com>
2011-11-16 10:03:30 -0600

    fixed color position issue

--- home/.bashrc_local/99_set_flexible_prompt.sh
+++ home/.bashrc_local/99_set_flexible_prompt.sh
@@ -1,6 +1,6 @@
 function set_flexible_prompt {
   local red="\[\033[31m\]" 
-  local green="\[\033[32m\]" 
+  local green="\\[\033[32m\\]" 
   local yellow="\[\033[33m\]" 
   local blue="\[\033[34m\]" 
   local clear="\[\033[0;0m\]"
